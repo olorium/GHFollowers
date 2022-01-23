5c6f5d13602e054a3475bd0f8f30dfdb0c395865
Alex Vasyliev <olorin2007@gmail.com>
2022-01-23 20:43:32 +0200

    Setup models

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Oleksii Vasyliev on 23.01.2022.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
