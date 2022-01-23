026b6ea8d73383881616345ada51d8c42eac5f0e
Alex Vasyliev <olorin2007@gmail.com>
2022-01-23 22:09:05 +0200

    prepare networking manager and proper error handling

--- GHFollowers/Screens/FollowersListVC.swift
+++ GHFollowers/Screens/FollowersListVC.swift
@@ -14,7 +14,20 @@ class FollowersListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result {
+            case .success(let followers):
+                print(followers)
+            case.failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
     }
 }
---
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessage.swift
+//  GHFollowers
+//
+//  Created by Oleksii Vasyliev on 23.01.2022.
+//
+
+import Foundation
+
+enum GFError: String, Error {
+    case invalidUsername = "This username created an invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponce = "Invalid response from the server. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+}
